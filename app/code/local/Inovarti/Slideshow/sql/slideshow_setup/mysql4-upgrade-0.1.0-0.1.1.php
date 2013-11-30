f0cc7997c3b9d2be2491155da44dc92bbfc3cb41
Isaac Lopes <contato@isaaclopes.com.br>
2013-11-30 17:11:04 -0200

    atualizar

--- app/code/local/Inovarti/Slideshow/sql/slideshow_setup/mysql4-upgrade-0.1.0-0.1.1.php
+++ app/code/local/Inovarti/Slideshow/sql/slideshow_setup/mysql4-upgrade-0.1.0-0.1.1.php
@@ -1,39 +1,30 @@
 <?php
+
 /**
  *
  * @category   Inovarti
  * @package    Inovarti_Slideshow
  * @author     Suporte <suporte@inovarti.com.br>
  */
+$installer = $this;
 
-$installer           = $this;
-$connection          = $installer->getConnection();
-
-$slidesTable          = $installer->getTable('slideshow/slides');
+$slidesTable = $installer->getTable('slideshow/slides');
 
 $installer->startSetup();
 
-
-
-$connection->modifyColumn(
-    $slidesTable,
-    'from_date',
-    array(
-        'type'      => Varien_Db_Ddl_Table::TYPE_DATE,
-        'nullable'  => true,
-        'default'   => null
-    )
+$installer->getConnection()
+        ->addColumn($slidesTable, 'from_date', array(
+            'type' => Varien_Db_Ddl_Table::TYPE_DATE,
+            'comment' => 'From date',
+            'nullable' => true,
+            'default' => null)
 );
-
-$connection->modifyColumn(
-    $slidesTable,
-    'to_date',
-    array(
-        'type'      => Varien_Db_Ddl_Table::TYPE_DATE,
-        'nullable'  => true,
-        'default'   => null
-    )
+$installer->getConnection()
+        ->addColumn($slidesTable, 'to_date', array(
+            'type' => Varien_Db_Ddl_Table::TYPE_DATE,
+            'comment' => 'To date',
+            'nullable' => true,
+            'default' => null)
 );
-
 $installer->endSetup();
 
