54c48e23e2f690a3fa6b61a7971171b46804644b
Isaac Lopes <contato@isaaclopes.com.br>
2013-12-01 21:19:38 -0200

    atualizar

--- app/code/local/Inovarti/Slideshow/Block/Slideshow.php
+++ app/code/local/Inovarti/Slideshow/Block/Slideshow.php
@@ -31,6 +31,7 @@ class Inovarti_Slideshow_Block_Slideshow extends Mage_Core_Block_Template {
         $slides = Mage::getModel('slideshow/slideshow')->getCollection()
                 ->addStoreFilter(Mage::app()->getStore())
                 ->addFieldToSelect('*')
+                ->addFieldToFilter('position', 0)
                 ->addFieldToFilter('status', 1)
                 ->setOrder('sort_order', 'asc');
 
@@ -45,5 +46,31 @@ class Inovarti_Slideshow_Block_Slideshow extends Mage_Core_Block_Template {
                         ), 'left');
         return $slides;
     }
+    public function getSlidesmini() {
+        $todayStartOfDayDate = Mage::app()->getLocale()->date()
+                ->setTime('00:00:00')
+                ->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
 
+        $todayEndOfDayDate = Mage::app()->getLocale()->date()
+                ->setTime('23:59:59')
+                ->toString(Varien_Date::DATETIME_INTERNAL_FORMAT);
+
+        $slides = Mage::getModel('slideshow/slideshow')->getCollection()
+                ->addStoreFilter(Mage::app()->getStore())
+                ->addFieldToSelect('*')
+                ->addFieldToFilter('position', 1)
+                ->addFieldToFilter('status', 1)
+                ->setOrder('sort_order', 'asc');
+
+        $slides
+                ->addFieldToFilter('from_date', array('or' => array(
+                        0 => array('date' => true, 'to' => $todayEndOfDayDate),
+                        1 => array('is' => new Zend_Db_Expr('null')))
+                        ), 'left')
+                ->addFieldToFilter('to_date', array('or' => array(
+                        0 => array('date' => true, 'from' => $todayStartOfDayDate),
+                        1 => array('is' => new Zend_Db_Expr('null')))
+                        ), 'left');
+        return $slides;
+    }
 }
